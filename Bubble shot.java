c8a9593412138228b13ac72c7245eeba29238fbb
Abinxv <140913718+Abinav37@users.noreply.github.com>
2024-08-02 17:32:12 +0530

    Update Bubble shot.java

--- Bubble shot.java
+++ Bubble shot.java
@@ -12,22 +12,27 @@ class bubbleshot
         }
         for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-i-1;j++)
+            for(int j=0;j<n-1;j++)
             {
-                if(arr[j]<arr[j+1])
+                if(arr[j]>arr[j+1])
                 {
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
-                System.out.println();
+                /*System.out.println();
             System.out.println("Iteration "+i+1+":");
             for(int k=0;k<n;k++)
             {
                 System.out.print(arr[k]+" ");
-            }
+            }*/
             }
             
         }
+         for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        
     }
 }
